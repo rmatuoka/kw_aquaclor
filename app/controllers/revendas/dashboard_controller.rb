56d913bd869b487cc8dc91964fb51bbdb8dc8d5d
Rafael Matuoka <rmatuoka@korewa.com.br>
2011-12-23 13:53:41 -0200

    ajustes

--- app/controllers/revendas/dashboard_controller.rb
+++ app/controllers/revendas/dashboard_controller.rb
@@ -3,7 +3,28 @@ class Revendas::DashboardController < ApplicationController
 	
 	def index
 	  #CALCULA O PRIMEIRO
+	  @Usuarios = ProductReceiptsUser.all(:group => :user_id)
+	  @Primeiro = 0
+	  @Counter = 0
+	  @Posicao = 0
+	  
+	  @Usuarios.each do |u|
+      @Counter = @Counter + 1
+      
+      @User = User.find(u.user_id)
+      @TotalUser = @User.product_receipts_users.all.count
+      
+      if @TotalUser > @Primeiro
+        @Primeiro = @TotalUser
+      end
+      
+      @Posicao = @Counter if @User.id == current_user.id
+
+    end
+    
+	  #CALCULA TOTAL DO USUARIO
 	  @TotalVendido = current_user.product_receipts_users.all.count
+	  
 	end
 	
 	def busca
@@ -16,14 +37,16 @@ class Revendas::DashboardController < ApplicationController
   
   def positivar
     !if !params[:p].blank?
-      @check = ProductReceiptsUser.first(:conditions => ['user_id = ? AND products_receipt_id = ?',current_user.id, params[:p].to_i])
-    
+      @check = ProductReceiptsUser.first(:conditions => ['products_receipt_id = ?', params[:p].to_i])
+      
       if !@check
         @positivacao = ProductReceiptsUser.new
         @positivacao.user_id = current_user.id
         @positivacao.products_receipt_id = params[:p].to_i
         @positivacao.save
       end
+      
+      @TotalVendido = current_user.product_receipts_users.all.count
     end
   end
 end
