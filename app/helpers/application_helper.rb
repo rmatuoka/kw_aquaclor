7683405c8626d302393f62e0f2c659de35a253d7
-_- Zaba -_- <bsilva@korewa.com.br>
2011-12-23 14:54:02 -0200

    Crud Produtos e Revendas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def print_active(status)  
+    retorno = ""
+    if status
+      retorno = "Sim"
+    else
+      retorno = "Não"
+    end
+    return retorno
+  end
 end
