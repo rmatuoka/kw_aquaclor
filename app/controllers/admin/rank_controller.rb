54fff04297642ad91d537809813546f04175c2c9
-_- Zaba -_- <bsilva@korewa.com.br>
2012-04-02 12:56:10 -0300

    Ajustes

--- app/controllers/admin/rank_controller.rb
+++ app/controllers/admin/rank_controller.rb
@@ -8,6 +8,14 @@ class Admin::RankController < ApplicationController
 												  :select=> "`product_receipts_users`.`user_id`, count(`product_receipts_users`.`products_receipt_id`) as `totalvendas`",
 												  :group => :user_id,
 												  :order=> " `totalvendas` desc, `product_receipts_users`.`user_id` ")
+		teste = ProductsReceipt.find(:all, 
+		                      :select => "`products_receipts`.*, COUNT(`products_receipts`.`amount`) as `totalprodutos`, `receipts`.`reseller_id`",
+		                      :joins=>  "INNER JOIN `receipts` ON `products_receipts`.`receipt_id` = `receipts`.`id`",
+		                      :group => "`receipts`.`reseller_id`")
+		@testearray = Array.new
+    teste.each do |t|
+      @testearray[t.reseller_id] = t.totalprodutos
+    end
 		@NotasCadastradas = Receipt.count
 		@ProdutosCadastrados = ProductsReceipt.count
 		@NotasValidadas = ProductReceiptsUser.count
